0f0418c17c6f202ac92b68c7ef60356e500d1474
dspeck1 <dspeck@burwood.com>
2025-06-11 13:25:56 -0500

    Update grants for rubin_rw

--- kubernetes/overlays/prod/sql/grants.sql
+++ kubernetes/overlays/prod/sql/grants.sql
@@ -15,5 +15,20 @@ GRANT SELECT ON public.obs_alterations_unassociations TO rubin;
 GRANT SELECT ON public.obs_sbn TO rubin;
 GRANT SELECT ON public.primary_objects TO rubin;
 
---rubin-admin
-GRANT SELECT, UPDATE, DELETE, INSERT ON ALL TABLES in SCHEMA public TO rubin_rw;
+--rubin_rw
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.current_identifications TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.mpc_orbits TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_els TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_events TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_obs TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_obs_archive TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_prev_des TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.neocp_var TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.numbered_identifications TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.obs_alterations_corrections TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.obs_alterations_deletions TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.obs_alterations_redesignations TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.obs_alterations_unassociations TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.obs_sbn TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.primary_objects TO rubin_rw;
+GRANT SELECT, UPDATE, DELETE, INSERT ON public.minor_planet_names TO rubin_rw;
